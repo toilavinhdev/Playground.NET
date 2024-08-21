dd746a35d7b0d7a0b6e059eacc83e56c815687a2
toilavinh <hoangdvinh68@gmail.com>
2024-08-21 07:30:09 +0700

    RabbitMQ

--- Playground.RabbitMQ.Consumer2/Program.cs
+++ Playground.RabbitMQ.Consumer2/Program.cs
@@ -2,25 +2,18 @@
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
-const string routingKey = "key-1";
+const string routingKey = "key-2";
 
 var factory = new ConnectionFactory { HostName = "localhost" };
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
-
-channel.QueueDeclare(
-    queue: routingKey,
-    durable: false, 
-    exclusive: false, 
-    autoDelete: false, 
-    arguments: null);
     
 var consumer = new EventingBasicConsumer(channel);
 
 consumer.Received += (model, ea) =>
 {
     var message = Encoding.UTF8.GetString(ea.Body.ToArray());
-    Console.WriteLine($"Consumer 1 received: {message}");
+    Console.WriteLine($"Consumer 2 received: {message}");
 };
 channel.BasicConsume(queue: routingKey,
     autoAck: true,
--- Playground.RabbitMQ/Program.cs
+++ Playground.RabbitMQ/Program.cs
@@ -4,33 +4,28 @@ using RabbitMQ.Client;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// ===============================================================
 
 // Setup
-var factory = new ConnectionFactory
-{
-    HostName = "localhost",
-};
-using var connection = factory.CreateConnection();
-using var channel = connection.CreateModel();
+var factory = new ConnectionFactory { HostName = "localhost" };
+var connection = factory.CreateConnection();
 
-const string queue = "queue-test-1";
-
-// Queues
-channel.QueueDeclare(
-    queue: queue,
-    durable: false, 
-    exclusive: false, 
-    autoDelete: false, 
-    arguments: null);
-
+// ===============================================================
 var app = builder.Build();
 app.UseSwagger().UseSwaggerUI();
-app.MapPost("/publish", (string message) =>
+app.MapPost("/publish", (string routingKey, string message) =>
 {
+    var channel = connection.CreateModel();
+    
+    var properties = channel.CreateBasicProperties();
+    properties.Persistent = false;
+    
     channel.BasicPublish(
         exchange: string.Empty,
-        routingKey: queue,
-        basicProperties: null,
+        routingKey: routingKey,
+        basicProperties: properties,
         body: Encoding.UTF8.GetBytes(message));
 });
 app.Run();
+
+// docker run -it --rm --name rabbitmq -d -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
